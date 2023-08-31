addac662a1538793913138e25bd54322ed938cb8
Will Martin <102479342+Royal021@users.noreply.github.com>
2023-08-30 21:01:25 -0400

    working with flipped letters

--- kmain.c
+++ kmain.c
@@ -14,12 +14,42 @@ __asm__(
 #include "memory.h"
 
 
+
+
 void kmain(){
     bss_init();
     serial_init();
     video_init();
+    unsigned cw = 10;
     video_draw_character('W', 100,200);
-    kprintf("We the People of the United States\n");
+    video_draw_character('e', 100+cw, 200);
+    video_draw_character('t', 100+cw*3, 200);
+    video_draw_character('h', 100+cw*4, 200);
+    video_draw_character('e', 100+cw*5, 200);
+    video_draw_character('P', 100+cw*7, 200);
+    video_draw_character('e', 100+cw*8, 200);
+    video_draw_character('o', 100+cw*9, 200);
+    video_draw_character('p', 100+cw*10, 200);
+    video_draw_character('l', 100+cw*11, 200);
+    video_draw_character('e', 100+cw*12, 200);
+    video_draw_character('o', 100+cw*14, 200);
+    video_draw_character('f', 100+cw*15, 200);
+    video_draw_character('t', 100+cw*17, 200);
+    video_draw_character('h', 100+cw*18, 200);
+    video_draw_character('e', 100+cw*19, 200);
+    video_draw_character('U', 100+cw*21, 200);
+    video_draw_character('n', 100+cw*22, 200);
+    video_draw_character('i', 100+cw*23, 200);
+    video_draw_character('t', 100+cw*24, 200);
+    video_draw_character('e', 100+cw*25, 200);
+    video_draw_character('d', 100+cw*26, 200);
+    video_draw_character('S', 100+cw*28, 200);
+    video_draw_character('t', 100+cw*29, 200);
+    video_draw_character('a', 100+cw*30, 200);
+    video_draw_character('t', 100+cw*31, 200);
+    video_draw_character('e', 100+cw*32, 200);
+    video_draw_character('s', 100+cw*33, 200); 
+    kprintf("\n DONE\n");
     while(1){
     }
 }
--- testharness.py
+++ testharness.py
@@ -4,6 +4,8 @@ import subprocess
 import sys
 import asyncio
 import asyncio.subprocess
+import base64,bz2
+import os
 
 async def testIt(P):
     for i in range(1000):
@@ -12,11 +14,33 @@ async def testIt(P):
         if not s:
             print("Process exited without printing required string")
             return
-        if s == "We the People of the United States\n":
-            print("Found expected line. Good.")
+        if s == "DONE\n":
+            await doImageComparison(P)
             return
     print("Never found expected output.")
 
+async def doImageComparison(P):
+    try:
+        os.unlink("output.ppm")
+    except FileNotFoundError:
+        pass
+    P.stdin.write(b"~c\nscreendump output.ppm\ninfo status\n")
+    await P.stdin.drain()
+    for i in range(100):
+        s = await readline(P)
+        if "VM status" in s:
+            break
+    else:
+        print("Something went wrong")
+        return
+
+    with open("output.ppm","rb") as fp:
+        actual = fp.read()
+    exp = bz2.decompress(base64.b64decode(expected))
+    if exp == actual:
+        print("Images match! Good!")
+    else:
+        print("Output does not match expected results")
 
 async def main():
     python = sys.executable
@@ -27,14 +51,16 @@ async def main():
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.STDOUT
     )
-    await testIt(P)
-    P.stdin.write(b"~cquit\n")
-    await P.stdin.drain()
-    await asyncio.sleep(0.5)
     try:
-        P.terminate()
-    except:
-        pass
+        await testIt(P)
+    finally:
+        P.stdin.write(b"\nquit\n\n~c\nquit\n")
+        await P.stdin.drain()
+        await asyncio.sleep(0.5)
+        try:
+            P.terminate()
+        except:
+            pass
 
     return
 
@@ -47,9 +73,23 @@ async def readline(P):
             return line.decode(errors="ignore")
         line += c
         if c == b'\n':
-            return line.decode(errors="ignore")
+            line = line.decode(errors="ignore")
+            print (line)
+            return line
         if len(line) > 16384:
             raise Exception("Too much data for one line")
 
+expected=(
+"QlpoOTFBWSZTWe8Gmp0AMLz/qdJZJIJJJNNJJAJABJBJIBIAQEAAAAQACMACfAAkgankTQKb"
+"VHqaepoaA0D0hhoZMgZGIMTJoaYMNDJkDIxBiZNDTAiREaTUTQZPSDQGhkgepuUa49YXvE5u"
+"7ZNtisSy9+DnjGk3CjJkoqSYMSDCPE4hckYwEqYmOEL41kF2gGouYyIsl42CUy0DGbLCtFEx"
+"taNxdRhvSZhUqiSSVRNyFkcQoxiirLugHbehdyOuHV4m+FpmxFROL1mi72yYJnFBUUtiooi8"
+"wRQRrNJyACipRYMhI74oqAWMkB3jxsKKCqdAUQioQUICwGIAeHf8cI4UBgDFSCBAgDFYpES4"
+"qEL69mFduteXwzaJ/YKiqJ0EUF7Dz7uuAEF8/Ts8tGjqeMLlQ8CrnJRR3R1BMuJIGPl7H87V"
+"f0qpcXW2C9dHxknNamSodEiZEJZcP1qwnWyUJsYSwKwhwhV4SB9eJNtSxYHoKU1MlmzCAmvk"
+"mcD5ILL6spfbKQ7aITIUOCnwo1dYztfaVgNdDz3CzCfxnuntPXub7eaQo7Smywl2/59vrZsZ"
+"hCdffzO/Jlv0rGNr3NvENOcgZD0G/Su8uPPn125PTQ6JVdB7N3OI1L6U8tWUyRna3JkatPvK"
+"IY7q8bMKkUobliG9zR4zjFbXVDSEE4dgVdnDEyRVf8XckU4UJDvBpqdA"
+)
 
 asyncio.run(main())
