759e08edce153e0bf0b80b261e3e8038d1e04496
Will Martin <102479342+Royal021@users.noreply.github.com>
2023-08-31 09:58:09 -0400

    morelogic, added testsuite

--- console.c
+++ console.c
@@ -20,14 +20,19 @@ static int max_col = 80;
 //continue; // NEXT WEEK
 //if(c=='\x7f')
 //{
+//if(row ==0 )
+//continue;
+//else
+//{
 //if(column >0)
 //col -=1; video_draw_character(' ');
 //else
 //row-=1; col = 79; video_draw_character(' ');
 //}
+//}
 //video_draw_character();
 //col+=1;
-//if(col==80)
+//if(col>=80)
 //{col=0; row++;}
 
 void console_putc(char ch)
