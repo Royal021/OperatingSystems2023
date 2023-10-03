bebc0e92c1d61a32ad9d954ed6978918950d4ba2
Will Martin <102479342+Royal021@users.noreply.github.com>
2023-10-03 09:49:47 -0400

    everything but lab started

--- make.py
+++ make.py
@@ -2,6 +2,7 @@ import configparser
 import subprocess
 import os
 import sys
+import lab11
 
 inifile=configparser.ConfigParser()
 inifile.read("config.ini")
@@ -54,17 +55,8 @@ for filename in os.listdir("."):
 
 doIt( [link] + linkflags + objectfiles )
 
-doIt( [python, "fool.zip", "sd.img",
-    "create", "64",
-    "cp", "about.txt", "ABOUT.TXT",
-    "cp", "article1.txt", "ARTICLE1.TXT",
-    "cp", "article2.txt", "ARTICLE2.TXT",
-    "cp", "article3.txt", "ARTICLE3.TXT",
-    "cp", "article4.txt", "ARTICLE4.TXT",
-    "cp", "article5.txt", "ARTICLE5.TXT",
-    "cp", "article6.txt", "ARTICLE6.TXT",
-    "cp", "billofrights.txt", "bill of rights.txt"
-])
+doIt( [python, "fool.zip", "sd.img", "create", "64"] )
+lab11.copy(python,"sd.img")
 
 doIt( [ qemu,
     "-m", "512",                #memory (512MB)
